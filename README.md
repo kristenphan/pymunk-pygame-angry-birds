6faf99d1992446fdee7e7173dca92ef3102cbcf5
Kristen Phan <kristenphan@KristenLaptop.localdomain>
2025-06-08 13:41:11 +0200

    add static L shape catching balls

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# angry-birds-finland
-Built with Python
+# Using Pymunk and Pygame to build Angry Birds
 
+## How to run project locally
 ```
 # Install poetry
 curl -sSL https://install.python-poetry.org | python3 -
@@ -20,7 +20,41 @@ poetry run python3 -m pymunk.examples -l
 
 # Run Pymunk example game
 poetry run python3 -m pymunk.examples.breakout
+
+# Run games built by Kristen
+poetry run python3 game-1-slide-and-pin-joint/main.py
 ```
+
+## About Pymunk and Pygame
+- Pymunk is for physics logics while Pygame is for rendering. 
+### Pymunk
+- [Docs](https://www.pymunk.org/en/latest/installation.html)
+- Built on top of a 2D physics library Munk2D written in C. 
+- Key concepts
+    - Rigid body
+        - A rigid body holds the physical properties of an object: mass, position, rotation, velocity, etc. 
+        - It does not have a shape by itself. 
+        - Example bodies: a football with a shape of circle in 2D, a wooden plank.
+    - Collision shape
+        - By attching shapes to bodies, you can define the body's shape. 
+        - You can attach multiple shapes to a single body to define a complex shape, or none if the body does not require a shape. 
+        - Example shapes: circle, square. 
+    - Constraint/ joint
+        - You can attach joints between 2 bodies to constrain their behavior. 
+    - Space
+        - A space is the basic simulation unit in Munk2D. 
+        - You add bodies, shapes, and joints to a shape, and then update the space as a whole. 
+- Physics properties
+    - Friction
+        - https://www.pymunk.org/en/latest/pymunk.html#pymunk.Circle.friction
+        - friction = 0 is frictionless (Can slide forever).
+    - Gravity
+        - https://www.pymunk.org/en/latest/pymunk.html#pymunk.Space.gravity
+        - Use Vec2d to set gravity to pull objects downwards
+            - y is positive downwards, x is positive to the right.
+            - (0, 900) pulls downwards with acceleration 900 pixels/second^2.
+    
+### Pygame
 
 ## Resources
 - https://python-poetry.org/docs/basic-usage/#using-poetry-run
--- game-1-slide-and-pin-joint/main.py
+++ game-1-slide-and-pin-joint/main.py
@@ -19,14 +19,23 @@ def main():
     # A basic simulation unit in Pymunk.
     # You add bodies, shapes, and joints to a space, and then update the space as a whole. 
     space = pymunk.Space()
-    space.gravity = (0, 900)  # Use Vec2d to set gravity to pull objects downwards
+
+    # Use Vec2d to set gravity to pull objects downwards
+    # Vec2d: y is positive downwards, x is positive to the right.
+    # Higher y means objects accelerate faster.
+    # https://www.pymunk.org/en/latest/pymunk.html#pymunk.Space.gravity
+    space.gravity = (0, 900)  
 
     balls = []
+    ticks_to_next_ball = 10 # Number of ticks before adding a new ball
+    lines = add_static_l(space)
+
+    # Use Pymunk's debugging utilities as a quick way to draw the space instead of using Pygame.
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-    ticks_to_next_ball = 10 # Number of ticks before adding a new ball
-
+    # Keep the game running until the user closes it
     while True:
+        # Process all user events such as keyboard, mouse, and window events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -34,17 +43,29 @@ def main():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit(0)
 
-            ticks_to_next_ball -= 1
-            if ticks_to_next_ball <= 0:
-                ticks_to_next_ball = 10
-                ball_shape = add_ball(space)
-                balls.append(ball_shape)
-            
-            screen.fill((255, 255, 255))  # Fill the screen with white color
-            space.step(1/50.0)  # Step the physics simulation
-            space.debug_draw(draw_options)  # Draw the space
-            pygame.display.flip()
-            clock.tick(60) # Set frame rate: 50 frames per second
+        # Spawn a new ball every 10 ticks of the loop
+        ticks_to_next_ball -= 1
+        if ticks_to_next_ball <= 0:
+            ticks_to_next_ball = 10
+            ball_shape = add_ball(space)
+            # balls.append(ball_shape)
+        
+        # Fill the screen with white color
+        screen.fill((255, 255, 255))  
+
+        # Step the physics simulation  
+        space.step(1/50.0)              
+
+        # Draw the space
+        # Simplier than using Pygame's drawing functions, but ignores shape colors.
+        space.debug_draw(draw_options)  
+
+        # Update the display with what you just drew
+        pygame.display.flip()           
+
+        # Set frame rate: 20 frames per second (controls how fast the while loop runs)
+        # Higher fps means the balls are spawned more frequently.
+        clock.tick(20)                 
 
 def add_ball(space):
     """
@@ -55,6 +76,8 @@ def add_ball(space):
     # It does not have a shape by itself. 
 
     # 1. Create ball's body at a starting position.
+    # Default body type is dynamic, meaning it can move and be affected by forces.
+    # https://www.pymunk.org/en/latest/_modules/pymunk/body.html#Body
     body = pymunk.Body() 
     x = random.randint(100, 500)
     body.position = x, 50 
@@ -69,6 +92,7 @@ def add_ball(space):
     # When a force hits an object at its center of mass, the object mostly just moves.
     # If it hits off-center, the object tends to rotate.
     # Easiest way to let Pymunk handle inertia calculation from mass.
+    # https://www.pymunk.org/en/latest/pymunk.html#pymunk.Shape.mass
     shape.mass = mass
 
     # 4. Make the ball roll by setting its friction.
@@ -76,9 +100,30 @@ def add_ball(space):
 
     # 5. Add the body and shape to the space. 
     # The body must always be added to the space before or at the same time as any shapes attached to it. 
+    # https://www.pymunk.org/en/latest/_modules/pymunk/space.html#Space.add
     space.add(body, shape)
 
     return shape
+
+def add_static_l(space):
+    """
+    Add two static vertical and horizontal lines
+    forming an L shape to the space for the balls to land on.
+    """
+    # Static body does not move
+    # https://www.pymunk.org/en/latest/_modules/pymunk/body.html#Body
+    body = pymunk.Body(body_type=pymunk.Body.STATIC)        
+    body.position = (300, 300)                              # Position of the static body
+
+    # Create two segments (lines) to form an L shape
+    # Horizontal line from (-150, 0) to (255, 0) with thickness 5
+    line1 = pymunk.Segment(body, (-150, 100), (255, 100), 5)    # Horizontal line
+
+    # # Vertical line
+    line2 = pymunk.Segment(body, (-150, 100), (-150, 50), 5)        
+    line1.friction = 1                                  # Friction for the horizontal line
+    line2.friction = 1              
+    space.add(body, line1, line2)                       # Add the static body and lines to the space  
 
 if __name__ == "__main__":
     sys.exit(main())
