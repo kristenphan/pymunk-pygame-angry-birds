919b7824059768e43713a460a5002341fdd2e51e
Kristen Phan <kristenphan@KristenLaptop.localdomain>
2025-06-09 00:29:37 +0200

    game-3: colored balls turn gray upon colliding with gray balls

--- game-3-colliding-balls/main.py
+++ game-3-colliding-balls/main.py
@@ -6,13 +6,19 @@ from pymunk import Vec2d
 from pymunk import Space
 import random
 
+collision_type_wall = 0
+collision_type_ball_dead = 1 # Balls with colision type 1 are dead with gray color. Balls with collision type of greater than 1 are alive with pastel color. 
+ball_alive_color = (163, 201, 166)  # Color of the ball: red
+ball_dead_color = (211,211,211)  # Color of the dead ball: red
+num_alive_balls = 5  # Number of alive balls, which will be created with collision type greater than 1
+
 class Ball():
     def __init__(self, 
                  x: int, 
                  y: int,
                  collision_type: int,
                  space: Space, 
-                 radius: int = 10,
+                 radius: int = 20,
                  density: int = 1, 
                  elasticity: float = 1,
                  color: tuple = (255, 0, 0)):
@@ -21,7 +27,7 @@ class Ball():
         self.x = x
         self.y = y
         self.body.position = Vec2d(self.x, self.y)
-        self.body.velocity = random.uniform(-800, 800), random.uniform(-800, 800)  # Random initial velocity for x- and y-axis
+        self.body.velocity = random.uniform(-300, 300), random.uniform(-300, 300)  # Random initial velocity for x- and y-axis
         self.shape = pymunk.Circle(self.body, radius)
         self.shape.density = density
         self.shape.elasticity = elasticity
@@ -31,18 +37,27 @@ class Ball():
     
     def draw(self, display):
         # https://www.pygame.org/docs/ref/draw.html#pygame.draw.circle
+        new_color = self.color  # Default color
+        if self.shape.collision_type == collision_type_ball_dead:
+            new_color = ball_dead_color
+        else:
+            new_color = ball_alive_color
+
         pygame.draw.circle(
             display, 
-            self.color,
+            new_color,
             (int(self.body.position.x), int(self.body.position.y)),
             self.radius)  # Draw the ball at its current position
+    def change_to_dead_state(self, arbiter, space, data):
+        self.shape.collision_type = collision_type_ball_dead
 
-class Floor():
+class Wall():
     def __init__(self, 
                  point_left: Vec2d, 
                  point_right: Vec2d, 
                  thickness: int, 
                  elasticity: float, 
+                 collision_type: int,
                  color: tuple, 
                  space: Space):
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
@@ -51,6 +66,7 @@ class Floor():
         self.thickness = thickness
         self.shape = pymunk.Segment(self.body, self.point_left, self.point_right, self.thickness)
         self.shape.elasticity = elasticity
+        self.shape.collision_type = collision_type
         self.color = color
         space.add(self.body, self.shape)
     def draw(self, display):
@@ -60,9 +76,6 @@ class Floor():
                          self.shape.a, 
                          self.shape.b, 
                          self.thickness) 
-        
-def collide(arbiter, space, data):
-    print("Collision detected between balls!")	
 
 def main():
     # 1. Set up Pymunk space
@@ -77,34 +90,39 @@ def main():
     background_color = (255, 255, 255)  # White background
 
     # 2. Create balls
-    ball_color = (163, 201, 166)  # Color of the ball: red
-    ball_radius = 30
     balls = [Ball(x=random.randint(0, screen_size),
                   y=random.randint(0, screen_size),
-                  collision_type=i,  # Unique collision type for each ball
-                  radius=ball_radius,
-                  color=ball_color,
-                  space=space) for i in range(2)]  # Create two balls with different collision types
+                  collision_type=i+2,  # Unique collision type for each ball
+                  color=ball_alive_color,
+                  space=space) for i in range(num_alive_balls)]  # Create two balls with different collision types
     
-    floor_color = (255, 223, 211)
-    floor_thickness = 20  # Thickness of the floor
-    floor_elasticity = 0.8  # Elasticity of the floor
+    balls.append(Ball(x=random.randint(0, screen_size),
+                        y=random.randint(0, screen_size),
+                        collision_type=collision_type_ball_dead,  
+                        color=ball_dead_color,
+                        space=space)) 
+    
+    wall_color = (255, 223, 211)
+    wall_thickness = 20  # Thickness of the floor
+    wall_elasticity = 0.8  # Elasticity of the floor
     corners = [(Vec2d(0, screen_size), Vec2d(screen_size, screen_size)),
                (Vec2d(0, 0), Vec2d(screen_size, 0)),
                (Vec2d(0, 0), Vec2d(0, screen_size)),
                (Vec2d(screen_size, 0), Vec2d(screen_size, screen_size))]
     
     # 3. Create floors acting as walls to contain the balls
-    floors = [Floor(point_left=point_left, 
+    walls = [Wall(point_left=point_left, 
                       point_right=point_right, 
-                      thickness=floor_thickness, 
-                      elasticity=floor_elasticity, 
-                      color=floor_color,  # Color of the floor: black
+                      thickness=wall_thickness, 
+                      elasticity=wall_elasticity, 
+                      collision_type=collision_type_wall,  # Collision type for the floor
+                      color=wall_color,  # Color of the floor: black
                       space=space) for point_left, point_right in corners]
     
-    # 4. Set up collision handlers
-    handler = space.add_collision_handler(0, 1)  # Collision handler for balls with collision type 0 and 1
-    handler.separate = collide  # Function to call when a collision is detected
+    # 4. Set up collision handlers for each of the alive balls
+    handlers = [space.add_collision_handler(collision_type_ball_dead, i+2) for i in range(num_alive_balls)]
+    for i, handler in enumerate(handlers):
+        handler.separate = balls[i].change_to_dead_state
 
     # 4. Keep the game running unless the user interrupts it
     while True:
@@ -121,7 +139,7 @@ def main():
         for ball in balls:
             ball.draw(display)
 
-        for floor in floors:
+        for floor in walls:
             floor.draw(display)
 
         # 6. Update the display
